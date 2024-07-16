8a6beeeab7f45f39692eb6e0fad7702ea83dd94f
NGUYEN DUC HUYNH <duchuynhnguyen1906@gmail.com>
2024-07-16 16:57:39 +0900

    chore: Disable spell checker and console logs in ESLint configuration, add styles for typing game page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,53 @@
+"use client";
+import { useState, useEffect } from "react";
+import { toRomaji } from "wanakana";
+import Style from "@styles/index.module.scss";
+import { keywords } from "@/utils/keyword";
+
 export default function Home() {
+	const [currentIndex, setCurrentIndex] = useState(0);
+	const [typedRomaji, setTypedRomaji] = useState("");
+	const [score, setScore] = useState(0);
+
+	const currentWord = keywords[currentIndex].word;
+	const currentKana = keywords[currentIndex].extra[0].kana;
+	const romajiToType = toRomaji(currentKana);
+
+	useEffect(() => {
+		const handleKeyPress = (event: KeyboardEvent) => {
+			const { key } = event;
+
+			if (romajiToType.startsWith(typedRomaji + key)) {
+				setTypedRomaji((prev) => prev + key);
+				if (typedRomaji + key === romajiToType) {
+					setScore((prev) => prev + 1);
+					setTypedRomaji("");
+					setCurrentIndex((prev) => (prev + 1) % keywords.length);
+				}
+			}
+		};
+
+		window.addEventListener("keydown", handleKeyPress);
+
+		return () => {
+			window.removeEventListener("keydown", handleKeyPress);
+		};
+	}, [typedRomaji, currentIndex, romajiToType]);
+
+	const renderRomajiToType = () => {
+		return romajiToType.split("").map((char, index) => (
+			<span key={index} style={{ color: index < typedRomaji.length ? "green" : "black" }}>
+				{char}
+			</span>
+		));
+	};
+
 	return (
-		<div>
-			<h1>Home</h1>
+		<div style={{ textAlign: "center", marginTop: "50px" }}>
+			<h1>Typing Game</h1>
+			<p>Current Word: {currentWord}</p>
+			<p>Romaji to type: {renderRomajiToType()}</p>
+			<p>Score: {score}</p>
 		</div>
 	);
 }
