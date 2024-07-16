d61dd963bbb8b8ebaf6b10017d99976653b1320f
NGUYEN DUC HUYNH <duchuynhnguyen1906@gmail.com>
2024-07-16 16:03:37 +0900

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,13 @@
+import "@styles/GlobalStyles.scss";
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="ja">
+			<body>{children}</body>
+		</html>
+	);
+}
