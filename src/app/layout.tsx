2074413fb77da469a463095e4a3fa6d6df8325ac
NGUYEN DUC HUYNH <duchuynhnguyen1906@gmail.com>
2024-07-25 12:07:13 +0900

    chore: Update metadata for SEO optimization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,20 @@
 import "@styles/GlobalStyles.scss";
+import { Metadata } from "next";
+export const metadata: Metadata = {
+	title: "WEB打 - タイピング速度を上げるウェブゲーム",
+	description: "タイピングの内容はWebに関連するキーワード。Webのコーディング基礎力向上するためのウェブゲームです。",
+	viewport: "width=device-width, initial-scale=1.0",
+	keywords: "タイピング, ウェブゲーム, コーディング, 学生, WEB打",
+	authors: [{ name: "居眠りぼーいず" }],
+	icons: {
+		icon: "/favicon.ico",
+	},
+	openGraph: {
+		title: "WEB打 - タイピング速度を上げるウェブゲーム",
+		description: "タイピングの内容はWebに関連するキーワード。Webのコーディング基礎力向上するためのウェブゲームです。",
+		url: "https://typing-game-eight-eta.vercel.app/",
+	},
+};
 
 export default function RootLayout({
 	children,
